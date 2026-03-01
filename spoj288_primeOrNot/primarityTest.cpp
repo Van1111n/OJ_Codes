d125be2565c4d19a5edc584882d1459392bb43b5
van1111n <lijiqian159@outlook.com>
2026-03-01 15:15:24 +0800

    Fixed miller rabin primarity test For i<1e9 it gets the same result as divition method

--- spoj288_primeOrNot/primarityTest.cpp
+++ spoj288_primeOrNot/primarityTest.cpp
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-//FIXME:
-
 LLU fast_mul(LLU a, LLU b, LLU p) {
     LLU c = (LLU)a * b - (LLU)((LLF)a / p * b + 0.5L) * p;
     return c < p ? c : c + p;
@@ -26,10 +24,10 @@ LLU fast_pow(LLU a, LLU n, LLU p) {
 const int base[12] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
 // these bases ensures n<2^64 that pass the test is certainly a prime
 bool miller_rabin_primarity_test(LLU n) {
-    if (n == 2)
-        return true;
-    if (n % 2 == 0)
-        return false;
+    if (n < 3 || n % 2 == 0)
+        return n == 2;
+    if (n % 3 == 0)
+        return n == 3;
     // form n-1 to u*2^t
     LLU t = 0;
     LLU u = n - 1;
@@ -40,27 +38,33 @@ bool miller_rabin_primarity_test(LLU n) {
     // select a base a, need to mod n
     for (int i = 0; i < 12; i++) {
         LLU a = base[i] % n;
-        if (a % n == 0 || a % n == 1 || a % n == n - 1)
-            continue;
-        // let v=a^u(mod n)，square n times，check the result mod n. +/-1 means prime.
+        if (a % n == 0 || a % n == 1 || a % n == n - 1) continue;
         LLU v = fast_pow(a, u, n);
-        for (int j = 0; j < t; j++) {
-            v = fast_pow(v, 2, n);
-            if (v != 1 && v != n - 1) {
-                return false;
-            }
+        if (v == 1) continue;
+        int s;
+        for (s = 0; s < t; ++s) {
+            if (v == n - 1) break;
+            v = fast_mul(v, v, n);
         }
+        if (s == t)
+            return false;
+    }
+    return true;
+}
+
+bool division_primarity_test(LLU n){
+    for (LLU i=2; i*i<=n; i++){
+        if (n%i==0) return false;
     }
     return true;
 }
 
 int main() {
-    int t;
-    LLU n;
-    scanf("%d", &t);
-    for (int i = 0; i < t; i++) {
-        scanf("%llu", &n);
-        miller_rabin_primarity_test(n) ? printf("YES\n") : printf("NO\n");
+    for (LLU i=2; i<__LONG_LONG_MAX__; i++){
+        if (division_primarity_test(i)!=miller_rabin_primarity_test(i)) {
+            printf("for %d: div:%d, mr:%d\n", i, division_primarity_test(i), miller_rabin_primarity_test(i));
+            getchar();
+        }
     }
     return 0;
 }
