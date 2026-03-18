3fcb618be6991dc6c301fb2a432ef3ee5c1ede03
van1111n <lijiqian159@outlook.com>
2026-03-18 17:44:21 +0800

    fixed: input format, overflow of d

--- luoguP1480_highPrecisionDivision/p1480.cpp
+++ luoguP1480_highPrecisionDivision/p1480.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 // calc a/b=c...r
 int main() {
-    int a[5001], c[5001];
+    int a[5005], c[5005];
     int b;
-    char temp;
-    int length=0;
+    string s;
+    int length = 0;
     // a[0]表示最高位，以此类推
-    while(1){
-        scanf("%c", &temp);
-        if (temp=='\n') break;
-        else a[length]=temp-'0';
-        length++;
-    }
-    a[length] = -1;
-    c[length] = -1;
+    cin >> s;
     scanf("%d", &b);
-    int r = 0, d = 0;
+    for (int i = 0; i < s.size(); i++) {
+        a[i] = s.at(i) - '0';
+    }
+    length = s.length();
+    a[length] = c[length] = -1;
+    long long r = 0, d = 0;
     // 每步中，d是除数，r是余数
     for (int i = 0; i < length; i++) {
         d = 10 * r + a[i];
@@ -28,9 +27,11 @@ int main() {
     // 跳过c的前导0再输出
     int i;
     for (i = 0; i < length; i++) {
-        if (c[i] != 0) break;
+        if (c[i] != 0)
+            break;
     }
-    if (i==length) printf("0");
+    if (i == length)
+        printf("0");
     for (; i < length; i++) {
         printf("%d", c[i]);
     }
