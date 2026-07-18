245cc205979f60e9c2c21a8606a9b17cf83d9fac
van1111n <lijiqian159@outlook.com>
2026-07-18 18:41:37 +0800

    fixed empty output

--- luoguP1106/p1106.cpp
+++ luoguP1106/p1106.cpp
@@ -3,13 +3,13 @@
 
 int main() {
     std::string s;
-    unsigned long long k;
+    int k;
     std::cin >> s >> k;
 
     // 使用'#'作为棍母字符
     int cur = 0;
     while (k > 0 && cur < s.length()) {
-        for (int i = 1; i <= std::min(k, s.length() - cur - 1); i++) {
+        for (int i = 1; i <= std::min(k, (int)(s.length() - cur - 1)); i++) {
             if (s[cur + i] < s[cur]) {
                 k-=i;
                 for (int j = cur; j < cur + i; j++) {
@@ -31,5 +31,14 @@ int main() {
         }
     }
 
-    std::cout << s;
+    int i = 0;
+    while (i < s.length()-1 && (s[i] == '#' || s[i] == '0')) {
+        ++i;
+    }
+    while (i<s.length()) {
+        if (s[i] != '#') {
+            std::cout << s[i];
+        }
+        ++i;
+    }
 }
